a1726698aa97c77976d2a6930036d9f95926fd34
AkshayKotekar <akshaykotkar10100@gmail.com>
2026-04-17 22:59:39 +0530

    Update anotherOne.py

--- anotherOne.py
+++ anotherOne.py
@@ -6,7 +6,7 @@ from PIL import Image as PILImage
 import os
 
 IMG_SIZE = 224 
-class_names = ['organic', 'plastic', 'metal', 'paper', 'glass'] 
+class_names = ['organic', 'plastic', 'paper', 'metal', 'glass'] 
 saved_model_path = "efficientnetb0_trash_classifiernew.keras"
 
 @st.cache_resource
